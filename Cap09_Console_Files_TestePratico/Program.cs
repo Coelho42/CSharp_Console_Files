3cecc0fd579ceb7361ad809db5b81c50e84e8353
Relaxu <goncalomiguelcoelho@gmail.com>
2017-11-06 10:25:03 +0000

    Add project files.

---
+++ Cap09_Console_Files_TestePratico/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap09_Console_Files_TestePratico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
