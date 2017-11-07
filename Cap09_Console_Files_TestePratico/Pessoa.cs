86d0616991dd9c6178e50c3ae863dee038d8e367
Relaxu <goncalomiguelcoelho@gmail.com>
2017-11-07 11:00:12 +0000

    Teste com erros

---
+++ Cap09_Console_Files_TestePratico/Pessoa.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+/*
+    Classes com construtores
+    Construtores são métodos especiais que permitem criar objetos com dados
+    Neste exemplo temos:
+        - 3 Atributos: Nome, idade e género
+        - Construtores: Default e 3 especificos
+        - Metodos Locais
+        - Getters & Setters
+
+    Finalmente: para os objetos de uma classe poderem ser serializaveis, 
+    temos que dotar a classe dessa capacidade:
+    Adicionamos o atirbuto [Serializable] sobre a declaração de CLASSE
+*/
+
+namespace Cap09_Console_Files_TestePratico
+{
+    [Serializable]          // Atributo especial para serializar as classes
+    class Pessoa
+    {
+
+        #region Atributos
+
+        private string nome;
+        private int idade;
+
+        #endregion
+
+        #region Construtores
+
+        /// <summary>
+        /// Construtor default
+        /// Nada devolve
+        /// Nada recebe
+        /// Se não for declarado pelo programador, o java cria-o por defeito
+        /// É por isso que podemos criar objetos das classes sem declarar um unico construtor -> Pessoa P = new Pessoa();
+        /// </summary>
+        public Pessoa()
+        {
+
+        }
+
+        /// <summary>
+        /// Construtor que recebe a string para o atributo nome
+        /// </summary>
+        /// <param name="name"> nome da pessoa </param>
+        public Pessoa(string name)
+        {
+            // com "nome" nos parametros -> this.nome = nome;
+            // ou com name nos parametros
+
+            nome = name;
+        }
+
+        /// <summary>
+        /// Construtor que recebe a string para o atributo nome
+        /// </summary>
+        /// <param name="name"> nome da pessoa </param>
+        /// <param name="age"> idade da pessoa </param>
+        public Pessoa(string name, int age)
+        {
+            // com "nome" nos parametros -> this.nome = nome;
+            // ou com name nos parametros
+
+            nome = name;
+            idade = age;
+        }
+
+        #endregion
+
+        #region Metodos Locais
+
+        /// <summary>
+        /// Produz um cumprimento e revela o seu nome em função do genero
+        /// </summary>
+        /// <returns> a string com o cumprimento </returns>
+        public String DizOla()
+        {
+            return "Olá eu sou o " + this.nome + " e tenho " + this.idade + " anos";
+        }
+
+        #endregion
+
+        #region Getters & Setters
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public int getIdade()
+        {
+            return idade;
+        }
+
+        public void setIdade(int idade)
+        {
+            this.idade = idade;
+        }
+
+        #endregion
+
+
+    }
+}
--- Cap09_Console_Files_TestePratico/Program.cs
+++ Cap09_Console_Files_TestePratico/Program.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Teste Prático do capitulo 7
+ * Cap07_Console_Files_TestePratico
+ * 
+ * Op1 - Com Collections e classes - Obrigatório o uso de ciclos forEach
+ * 
+ * Teste: Elaborar a seguinte app.
+ * 
+ * - A unidade de dados base da app é uma classe ou struct Pessoa com os seguintes atributos
+ *      - Nome
+ *      - Idade
+ *      
+ * - Menu com 4 opções 
+ *      - Op1 - Recolhe os dados de uma pessoa, para um objeto Pessoa e adiciona à collection ou array de objetos
+ *      - Op2 - Mostra os dados da collection ou array no ecrã
+ *      - Op3 - Save: Guarda a collection ou array numa file binária
+ *      - Op4 - Load: Lê a collection ou array da file binária e mostra no ecrã
+ */
+
 namespace Cap09_Console_Files_TestePratico
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 } 
